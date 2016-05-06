00137e4a49ea92c6428d6f134516cfa18f2fca91
Kacper Lukawski <ka.lukawski@gmail.com>
2016-05-06 15:21:35 +0000

    changed description for undefiend

--- public/scripts/champions.js
+++ public/scripts/champions.js
@@ -16,7 +16,7 @@ function champions() {
               var winrate = Number((value.winrate * 100).toFixed(0));
               console.log(winrate);
               if(value.games === undefined){
-                $('#championsTable').append('<tr><td>' + counter + '</td> <td class="mdl-data-table__cell--non-numeric"> <img src="http://ddragon.leagueoflegends.com/cdn/6.8.1/img/champion/' + value.championKey + '.png">' + value.championName + '</td> <td>' + value.championLevel + '</td> <td>' + value.championPoints + '</td> <td colspan="3" style="text-align: center;"> 0 RANKED GAMES PLAYED </td></tr>');
+                $('#championsTable').append('<tr><td>' + counter + '</td> <td class="mdl-data-table__cell--non-numeric"> <img src="http://ddragon.leagueoflegends.com/cdn/6.8.1/img/champion/' + value.championKey + '.png">' + value.championName + '</td> <td>' + value.championLevel + '</td> <td>' + value.championPoints + '</td> <td colspan="3" style="text-align: center;"> No ranked games found this season </td></tr>');
               } else {
                 $('#championsTable').append('<tr><td>' + counter + '</td> <td class="mdl-data-table__cell--non-numeric"> <img src="http://ddragon.leagueoflegends.com/cdn/6.8.1/img/champion/' + value.championKey + '.png">' + value.championName + '</td> <td>' + value.championLevel + '</td> <td>' + value.championPoints + '</td> <td>' + value.games + '</td> <td>' + winrate + '%</td><td>' + value.kda + '</td></tr>');
               }
