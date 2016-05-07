fcc91bf798bb24eb9b1a9d30382ad1270f95f7bf
Kacper Lukawski <ka.lukawski@gmail.com>
2016-05-07 17:29:51 +0000

    added roles list

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   total();
-  //champions();
+  champions();
   roles();
 });
 
@@ -36,7 +36,6 @@ function champions() {
             };
             if(r.success){
                 $.each(r.champions, function (key, value) {
-                    console.log(value.name);
                     $('#champions').append("<li class='role mdl-list__item " + positions[counter] + "'><span class='mdl-list__item-primary-content'><img src='http://ddragon.leagueoflegends.com/cdn/6.8.1/img/champion/" + value.key + ".png' class='icon-responsive'> " + value.name + "</span><span>" + value.points + "</span></li>");
                     counter++;
                     if(counter > 2){
@@ -63,6 +62,13 @@ function roles() {
             };
             if(r.success){
                 console.log(r);
+                $.each(r.roles, function(key, value){
+                    $('#roles').append("<li class='role mdl-list__item " + positions[counter] + "'><span class='mdl-list__item-primary-content'><img src='/static/roles/" + key.toLowerCase() + ".png' class='img-responsive'>" + key + "</span><span>" + value + "</span></li>");
+                    counter++;
+                    if(counter > 2){
+                        return false;
+                    }
+                });
             }
         }
     });    
